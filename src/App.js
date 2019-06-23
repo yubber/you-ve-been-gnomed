89844caa11ff4033b09e268fbc77c32d9e32d3ee
yubber <39871798+yubber@users.noreply.github.com>
2019-06-23 14:28:35 +0800

    Update App.js
    worked on api fetch + device on/off handler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@material/react-card/dist/card.min.css';
+import Card, {
+  CardPrimaryContent,
+  CardMedia,
+  CardActions,
+  CardActionButtons,
+  CardActionIcons
+} from "@material/react-card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let apiget = async (request) =>
+  await fetch(`https://api.wifiplug.co.uk/v1.0/${request}/`, {
+    method:'GET',
+    headers: {
+      'content-type':'application/json',
+      'X-Api-Key':key,
+      'X-Api-Secret':secret // import from another file
+    }
+  }).then((e)=>e.json)
+
+let DevicePanel = (props) => {
+
+  let [powerstate, setpower] = useState()
+  return(<div> {/*waiting on christian for this component, but coding handlers first. i assume elliot is tinkering with api*/}
+    <button onClick={setpower(!powerstate)}></button> 
+  </div>)}
+
+let App = () => {
+  let devices = useEffect()
+  return(<div>
+    a ah aaa
+    {devices.map(e => (DevicePanel({
+      image: /*e.image*/"https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FG%2F01%2Faplusautomation%2Fvendorimages%2Ffce10a33-e801-409a-a4aa-e413ab856f31.jpg._CB304891446_.jpg&f=1",
+      title: e.name
+    })))}
+  </div>
+)}
 
 export default App;
